1d60a1e61c135cb336a1b1045f8daf9e639c801f
z <za1a2ew44zz_999@hotmail.com>
2026-06-04 14:54:34 +0700

    winsock init

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
-Custom HTTP Web Server
+#include<iostream>
+#include<winsock2.h>
+
+#pragma comment(lib,"ws2_32.lib")
+
+int main(){
+    
+    WSADATA wsaData;
+
+    std::cout << "init winsock+++" << std::endl;
+    if(WSAStartup(MAKEWORD(2,2), &wsaData) != 0){
+        std::cout << "failed to init winsock++ , Error code: " << WSAGetLastError() << std::endl;
+    }
+
+    std::cout << "Winsock++ init successfully" << std::endl;
+
+
+
+    WSACleanup();
+    return 0;
+}
